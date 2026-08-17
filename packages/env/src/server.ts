fac998e737750c90719a7dc64c88ec77a8a5a30f
Anderson Luis Silva <andersonluis87@gmail.com>
2026-08-17 13:30:15 +0200

    chore: setting up prisma models and seed

--- packages/env/src/server.ts
+++ packages/env/src/server.ts
@@ -3,12 +3,24 @@ import { createEnv } from "@t3-oss/env-core";
 import { z } from "zod";
 
 export const env = createEnv({
-  server: {
-    DATABASE_URL: z.string().min(1),
-    CORS_ORIGIN: z.url(),
-    NODE_ENV: z.enum(["development", "production", "test"]).default("development"),
-  },
-  runtimeEnv: process.env,
-  skipValidation: !!process.env.SKIP_ENV_VALIDATION,
-  emptyStringAsUndefined: true,
+	server: {
+		// API and DB configuration
+		SERVER_PORT: z.coerce.number().default(3333),
+		CORS_ORIGIN: z.url(),
+		DATABASE_URL: z.url(),
+
+		// Auth
+		JWT_SECRET: z.string().min(1),
+		GITHUB_CLIENT_ID: z.string().min(1),
+		GITHUB_CLIENT_SECRET: z.string().min(1),
+		GITHUB_REDIRECT_URI: z.url(),
+
+		// Logging
+		NODE_ENV: z
+			.enum(["development", "production", "test"])
+			.default("development"),
+	},
+	runtimeEnv: process.env,
+	skipValidation: !!process.env.SKIP_ENV_VALIDATION,
+	emptyStringAsUndefined: true,
 });
